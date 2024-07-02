731b0d39504d4f4458ac7c6c19fb98624b9ce566
nithinK-142 <nithin.sagar.359@gmail.com>
2024-07-02 14:11:27 +0530

    add: cors.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
